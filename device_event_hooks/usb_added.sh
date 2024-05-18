418c507809103ba88cfa36b3b0f64847ea82dfef
Shuhei <sh7916@gmail.com>
2024-05-18 19:04:44 +0900

    remove device_event_hooks/

--- device_event_hooks/usb_added.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-SCRIPT_PATH=$(readlink -f "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-
-DEVICE_INFO_LIST="$1"
-
-$SCRIPT_DIR/../tools/change_home_monitor_channel.sh "$DEVICE_INFO_LIST" 4
