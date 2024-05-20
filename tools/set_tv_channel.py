2b61ffdd9836865e49d4abcc25a8823a9e8b5deb
Shuhei <sh7916@gmail.com>
2024-05-20 21:51:45 +0900

    reuse http connection

--- tools/set_tv_channel.py
+++ tools/set_tv_channel.py
@@ -21,6 +21,7 @@ class SwitchBotAPI:
     def __init__(self, token, secret):
         self.token = token
         self.secret = secret
+        self.conn = http.client.HTTPSConnection(API_BASE_URL)
 
     def get_devices(self):
         nonce = uuid.uuid4()
@@ -39,9 +40,8 @@ class SwitchBotAPI:
             'nonce': str(nonce)
         }
 
-        conn = http.client.HTTPSConnection(API_BASE_URL)
-        conn.request("GET", f"/{API_VERSION}/devices", headers=apiHeader)
-        res = conn.getresponse()
+        self.conn.request("GET", f"/{API_VERSION}/devices", headers=apiHeader)
+        res = self.conn.getresponse()
         data = res.read()
         devices = json.loads(data.decode("utf-8"))['body']['infraredRemoteList']
         return devices
@@ -63,9 +63,8 @@ class SwitchBotAPI:
             'Content-Type': CONTENT_TYPE,
             'charset': CHARSET
         }
-        conn = http.client.HTTPSConnection(API_BASE_URL)
-        conn.request("POST", f"/{API_VERSION}/devices/{device_id}/commands", payload, apiHeader)
-        res = conn.getresponse()
+        self.conn.request("POST", f"/{API_VERSION}/devices/{device_id}/commands", payload, apiHeader)
+        res = self.conn.getresponse()
         data = res.read()
         return data.decode("utf-8")
 
