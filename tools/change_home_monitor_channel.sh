9b12f3eb98bb7e8b8227306849f04b2a9139d963
Shuhei <sh7916@gmail.com>
2024-05-25 12:25:43 +0900

    minor fix

--- tools/change_home_monitor_channel.sh
+++ tools/change_home_monitor_channel.sh
@@ -8,9 +8,6 @@ fi
 DEVICE_INFO_LIST="$1"
 ADD_OR_REMOVE="$2"
 
-[ "$ADD_OR_REMOVE" = "add" ] && CHANNEL=4
-[ "$ADD_OR_REMOVE" = "remove" ] && CHANNEL=2
-
 USB_HUB_ID="2109:2817"
 
 # load SWITCHBOT_TOKEN, SWITCHBOT_SECRET
@@ -19,4 +16,7 @@ source ~/.profile_env
 echo "$DEVICE_INFO_LIST" | grep -q "$USB_HUB_ID" || { echo "usb hub '$USB_HUB_ID' is not detected."; exit 0; }
 
 echo "usb hub '$USB_HUB_ID' is detected."
+
+[ "$ADD_OR_REMOVE" = "add" ] && CHANNEL=4
+[ "$ADD_OR_REMOVE" = "remove" ] && CHANNEL=2
 ~/automations/tools/set_tv_channel.py "01-202309241713-48053111" "$CHANNEL" # モニター
