bed3390ac591d78f98c098c6bec21e4283a5a64a
Shuhei <sh7916@gmail.com>
2024-06-19 23:54:47 +0900

    change display size: 2304x1296 => 2560x1440

--- tools/adjust_display_config.sh
+++ tools/adjust_display_config.sh
@@ -17,14 +17,14 @@ res="$(system_profiler -json SPDisplaysDataType)"
 count=$(echo "$res" | $JQ '.SPDisplaysDataType[].spdisplays_ndrvs | length')
 [ $count -eq 2 ] || exit 0
 
-# 意図したディスプレイサイズ(2304x1296を最適に表示できるサイズ≒4K)でない場合は何もしない
+# 意図したディスプレイサイズ(後に設定するサイズを最適に表示できるサイズ≒4K)でない場合は何もしない
 external_display_pixels="$(echo "$res" | $JQ -r '.SPDisplaysDataType[].spdisplays_ndrvs[] | select(._name != "Color LCD") | ._spdisplays_pixels')"
 [ "$external_display_pixels" = "3840 x 2160" -o "$external_display_pixels" = "4608 x 2592" ] || exit 0
 
 # MacBook Built-in Displayでは無い方のPersistent IDを取得
 external_display_persistent_id=$($DISPLAYPLACER list | grep "Persistent screen id:" | grep -v "$macbook_display_persistent_id" | awk '{print $4}')
 
-# ディスプレイを2304x1296に設定してミラーする（既に設定済みの場合は何もしない）
-config="id:$external_display_persistent_id+$macbook_display_persistent_id res:2304x1296 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0"
+# ディスプレイを設定してミラーする（既に設定済みの場合は何もしない）
+config="id:$external_display_persistent_id+$macbook_display_persistent_id res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0"
 $DISPLAYPLACER list | grep -q "$config" && exit 0
 $DISPLAYPLACER "$config"
